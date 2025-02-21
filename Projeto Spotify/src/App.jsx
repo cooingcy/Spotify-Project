b9c4e6589ec249622f6fcffbbc6468a9ff5801e4
cooingcy <matheuscurciromano@gmail.com>
2025-02-21 17:47:10 -0300

    Começando o back-end

--- Projeto Spotify/src/App.jsx
+++ Projeto Spotify/src/App.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import Header from "./components/Header";
-import Main from "./components/Main";
+import Home from "./Pages/Home";
+import Artists from "./Pages/Artists";
+import Artist from "./Pages/Artist";
+import Songs from "./Pages/Songs";
+import Song from "./Pages/Song";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Main />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/artists" element={<Artists />}/>
+        <Route path="/artist/:id" element={<Artist />}/>
+        <Route path="/songs" element={<Songs />}/>
+        <Route path="/song/:id" element={<Song />}/>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- Projeto Spotify/src/components/Main.jsx
+++ Projeto Spotify/src/components/Main.jsx
@@ -1,75 +1,26 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import ItemList from "./ItemList";
+import { artistArray } from "../assets/database/artists";
+import { songsArray } from "../assets/database/songs";
 
 const Main = () => {
   return (
     <div className="main">
-      <div className="item-list">
-        <div className="item-list__header">
-          <h2>Artistas Populares</h2>
-          <a className="item-list__link" href="/">
-            Mostrar tudo
-          </a>
-        </div>
+      <ItemList
+        title="Artistas"
+        items={5}
+        itemsArray={artistArray}
+        path="/artists"
+        idPath="/artists"
+      />
 
-        <div className="single-item__container">
-          <div className="single-item">
-            <div className="single-item__div-image-button">
-              <div className="single-item__div-image">
-                <img
-                  className="single-item__img"
-                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
-                  alt="Imagem do Henrique & Juliano"
-                />
-              </div>
-              <FontAwesomeIcon
-                className="single-item__icon"
-                icon={faCirclePlay}
-              />
-            </div>
-            <div className="single-item__texts">
-              <div className="single-item__2lines">
-                <p className="single-item__title">Henrique & Juliano</p>
-                <p className="single-item__type">Artista</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="item-list">
-        <div className="item-list__header">
-          <h2>Músicas Populares</h2>
-          <a className="item-list__link" href="/">
-            Mostrar tudo
-          </a>
-        </div>
-
-        <div className="single-item__container">
-          <div className="single-item">
-            <div className="single-item__div-image-button">
-              <div className="single-item__div-image">
-                <img
-                  className="single-item__img"
-                  src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24"
-                  alt="Última Saudade - Ao Vivo"
-                />
-              </div>
-              <FontAwesomeIcon
-                className="single-item__icon"
-                icon={faCirclePlay}
-              />
-            </div>
-            <div className="single-item__texts">
-              <div className="single-item__2lines">
-                <p className="single-item__title">Henrique & Juliano</p>
-                <p className="single-item__type">Artista</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <ItemList
+        title="Músicas"
+        items={10}
+        itemsArray={songsArray}
+        path="/songs"
+        idPath="/song"
+      />
     </div>
   );
 };
